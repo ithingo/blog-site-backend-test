21bed26aadf89b2ed2eff83ea80fb3a52b0d5fe1
Vlad Borodaev <nikoskefali@gmail.com>
2018-07-14 01:14:11 +0300

    Switched from sqlite to postgres

--- blog_rest/settings.py
+++ blog_rest/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'blog_rest.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'blog_rest',
+        'USER': 'blog_rest_user',
+        'PASSWORD': 'love_python',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
